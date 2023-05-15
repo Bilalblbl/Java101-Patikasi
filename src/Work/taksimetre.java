55100490141129f3d53d80a4b31f30ac04c0277b
Bilalblbl <bulbulbilal03@gmail.com>
2023-05-15 15:51:34 +0300

    Taksimetre Hesaplayan Program Tamamlandı

--- src/Work/taksimetre.java
+++ src/Work/taksimetre.java
@@ -3,19 +3,18 @@ package Work;
 import java.util.Scanner;
 public class taksimetre {
     public static void main(String[] args) {
-
-        int a;
-        double toplam;
+        
+        double a,toplam;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Gidilen Mesafe = ");
-        a = input.nextInt();
+        a = input.nextDouble();
 
         toplam = (a * 2.20) + 10 ;
 
-        //boolean srt = toplam > 20 ; 
+        //boolean srt = toplam > 20 ;
         toplam = toplam > 20 ? toplam : 20;
 
-        System.out.println("Toplam Tutar = " + toplam + "TL");
+        System.out.println("Toplam Tutar = " + toplam + " TL");
     }
 }
