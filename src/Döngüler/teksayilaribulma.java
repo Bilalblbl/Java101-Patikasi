36eb939fddf8e2bc93031199581075a6f94155fd
Bilalblbl <bulbulbilal03@gmail.com>
2023-07-13 11:03:20 +0300

    Çift ve 4'ün katları olan Sayıları toplayan Program Tamamladı

---
+++ src/Döngüler/teksayilaribulma.java
@@ -0,0 +1,22 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class teksayilaribulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a;
+        int b = 0;
+
+        do {
+            System.out.print("Sayı Giriniz = ");
+            a = input.nextInt();
+            if (a % 2 == 0 || a % 4 == 0) {
+                b += a;
+            }
+        }while (a % 2 == 0); System.out.println (a + " Tek sayı olduğundan işlem sonlandırıldı.");;
+
+        System.out.println("Girilen Çift Sayıların Toplamı = " + b);
+    }
+}
