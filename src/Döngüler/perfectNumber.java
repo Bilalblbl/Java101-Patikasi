0e291ce8fb6919391efa602783cb27ad1ac1d5ae
Bilalblbl <bulbulbilal03@gmail.com>
2023-08-02 15:04:39 +0300

    Mükemmel Sayı Bulan Program Tamamlandı

---
+++ src/Döngüler/perfectNumber.java
@@ -0,0 +1,25 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class perfectNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a, total = 0;
+
+        System.out.print("Sayı Giriniz ; ");
+        a = input.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0 && i != a) {
+                total += i;
+            }
+        }
+       if (total == a){
+           System.out.print(a + " bir mükemmel sayıdır çünkü pozitif tam sayı çarpanları (kalansız bölenleri) olan sayıların toplamı " + total + " olur.");
+       }else {
+           System.out.println(a + " bir mükemmel sayı değildir çünkü pozitif tam sayı çarpanları (kalansız bölenleri) olan sayıların toplamı " + total + " olur.");
+       }
+    }
+}
