8428be6191d56b19d02d069b132f506998f93ea1
Bilalblbl <bulbulbilal03@gmail.com>
2023-08-02 11:15:48 +0300

    Ebob Ekok bulan program tamamlandı

---
+++ src/Döngüler/findebobekok.java
@@ -0,0 +1,59 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class findebobekok {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n1, n2, i = 1, s = 1, ebob = 0, ekok = 0;
+
+        System.out.print("n1 sayısını giriniz : ");
+        n1 = input.nextInt();
+
+        System.out.print("n2 sayısını giriniz : ");
+        n2 = input.nextInt();
+
+//        if (n1 < n2) {
+//            while (i <= n1) {
+//                if (n1 % i == 0 && n2 % i == 0)
+//                    ebob = i;
+//                i++;
+//            }
+//            System.out.println(ebob);
+//        } else if (n2 < n1) {
+//            while (i <= n2) {
+//                if (n1 % i == 0 && n2 % i == 0)
+//                    System.out.println(i);
+//                    ebob = i;
+//                i++;
+//            }
+//
+//        }
+
+        int k = n1;
+        if (n1 < n2) {
+        while (k >= 1) {
+            if (n1 % k == 0 && n2 % k == 0) {
+                ebob = k;
+                System.out.println("Girilen rakamların Ebob'u : " + ebob);
+                break;
+            }
+            k--;
+        }
+        } else if (n2 < n1) {
+            while (k >= 1) {
+                if (n1 % k == 0 && n2 % k == 0) {
+                    ebob = k;
+                    System.out.println("Girilen rakamların Ebob'u : " + ebob);
+                    break;
+                }
+                k--;
+            }
+        }
+            System.out.println();
+            ekok = (n1 * n2) / ebob;
+            System.out.println("Girilen rakamların Ekok'u : " + ekok);
+
+    }
+}
