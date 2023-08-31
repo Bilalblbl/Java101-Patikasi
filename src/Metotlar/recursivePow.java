f2a74dd572d35e23cb27c2310a876f3ddb9bc8c2
Bilalblbl <bulbulbilal03@gmail.com>
2023-08-31 11:33:02 +0300

    Recursive Metotlar ile Üslü Sayı Hesaplama Tamamlandı

--- src/Metotlar/recursivePow.java
+++ src/Metotlar/recursivePow.java
@@ -2,24 +2,26 @@ package Metotlar;
 import java.util.Scanner;
 
 public class recursivePow {
-  static void us(){
+  static int f(int a,int b){
+
+    int result = 1;
+    if (a == 0 || b == 0){
+      System.out.println("\n0 girilemez");
+    }else {
+    for (int i = 1 ; i <= b; i++){
+      result *= a;
+    }
+    System.out.println("\nSonuç : " + result);}
+      return result;
+  }
+
+  public static void main(String[] args) {
     Scanner tnum = new Scanner(System.in);
     System.out.print("Taban Değeri Giriniz : ");
     int taban =  tnum.nextInt();
     System.out.print("Üs Değeri Giriniz : ");
     int üs =  tnum.nextInt();
-    int result = 1;
+    f(taban,üs);
 
-    if (üs == 0 || taban == 0){
-      System.out.println("\n0 girilemez");
-    }else {
-    for (int i = 1 ; i <= üs; i++){
-      result *= taban;
-    }
-    System.out.println("\nSonuç : " + result);}
   }
-
-    public static void main(String[] args) {
-    us();
-    }
 }
